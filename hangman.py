9f8db83dd5b38f4535ad06b2552589523c8a5781
Vladimir <shikarno_3000@inbox.ru>
2020-07-30 23:48:55 +0300

    empty lines added

--- hangman.py
+++ hangman.py
@@ -7,9 +7,12 @@ import random
 
 print('H A N G M A N')
 print('Guess the word:')
+
 languages_list = ['python', 'java', 'kotlin', 'javascript']
+
 right_choice = random.choice(languages_list)
 player_choice = input()
+
 if right_choice == player_choice:
     print('You survived!')
 else:
