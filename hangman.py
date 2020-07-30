5f2680c5e12e1ab6e03fb79ee281f446d42cd49a
Vladimir <shikarno_3000@inbox.ru>
2020-07-30 23:38:16 +0300

    here we go

---
+++ hangman.py
@@ -0,0 +1,16 @@
+'''
+This is a popular game. It called "H A N G M A N"
+'''
+
+import random
+
+
+print('H A N G M A N')
+print('Guess the word:')
+languages_list = ['python', 'java', 'kotlin', 'javascript']
+right_choice = random.choice(languages_list)
+player_choice = input()
+if right_choice == player_choice:
+    print('You survived!')
+else:
+    print('You are hanged!')
