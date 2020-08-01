e0976876dc5cd4b1ce6c7de5983d1a6459777f95
Vladimir <shikarno_3000@inbox.ru>
2020-08-01 15:25:48 +0300

    second step. 3 letters are displayed now

--- hangman.py
+++ hangman.py
@@ -4,15 +4,14 @@ This is a popular game. It called "H A N G M A N"
 
 import random
 
+languages_list = ['python', 'java', 'kotlin', 'javascript']
+right_choice = random.choice(languages_list)
+three_open_letters_word = (right_choice[:3] + '-' * (len(right_choice) - 3)).strip()
 
 print('H A N G M A N')
-print('Guess the word:')
+print(f'Guess the word: {three_open_letters_word}')
 
-languages_list = ['python', 'java', 'kotlin', 'javascript']
-
-right_choice = random.choice(languages_list)
 player_choice = input()
-
 if right_choice == player_choice:
     print('You survived!')
 else:
